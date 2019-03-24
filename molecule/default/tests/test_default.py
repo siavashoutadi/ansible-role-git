5dec88d48cf3992e4f123a855cc5d2a52214a38a
Siavash Outadi <siavash.outadi@gmail.com>
2019-03-24 02:46:55 +0100

    Add molecule tests

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_git_installed(host):
+    git = host.package("git")
+    assert git.is_installed
+
+
+def test_git_config(host):
+    sys_config = host.file("/etc/gitconfig")
+    sys_content = ['[alias]', '	co = checkout', '	st = status']
+    assert sys_config.content_string == '\n'.join(sys_content)
+    global_content = ['[user]', '	name = User1', '	email = user1@mail.com']
+    assert host.file(
+        "/root/.gitconfig").content_string == '\n'.join(global_content)
